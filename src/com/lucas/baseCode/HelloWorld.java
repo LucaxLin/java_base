16c5209d4dd4eeaa2cd15defac7be994e5c3ec56
lucaslin <892789989@qq.com>
2022-07-25 18:53:53 +0800

    add comment

--- src/com/lucas/baseCode/HelloWorld.java
+++ src/com/lucas/baseCode/HelloWorld.java
@@ -2,6 +2,7 @@ package com.lucas.baseCode;
 
 public class HelloWorld {
     public static void main(String[] args) {
+//        print hello world
         System.out.println("Hello world!");
     }
 }
